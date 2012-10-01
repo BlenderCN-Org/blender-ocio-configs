85493357da02412b450d5273b76ed9ab80f3c5e7
Troy James Sobotka <troy.sobotka@gmail.com>
2012-09-30 19:41:12 -0700

    Added a 1D clamping LUT to clamp transforms out of XYZ. If an input gamut is wider than an output gamut, the XYZ transforms will produce numbers that are negative and greater than 1.0. In the case of the latter, the values are effectively meaningless in an HDR model system, where greater than 1.0 values merely represent the same colour primaries at a greater luminosity.

--- make.py
+++ make.py
@@ -24,9 +24,7 @@ def Fit(value, fromMin, fromMax, toMin, toMax):
         raise ValueError("fromMin == fromMax")
     return (value - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin
 
-
 ###############################################################################
-
 
 config = OCIO.Config()
 config.setSearchPath('luts')
@@ -43,7 +41,6 @@ config.setRole(OCIO.Constants.ROLE_COLOR_PICKING,"sRGB")
 config.setRole(OCIO.Constants.ROLE_MATTE_PAINT,"Raw / Data")
 config.setRole(OCIO.Constants.ROLE_TEXTURE_PAINT,"sRGB")
 
-
 ###############################################################################
 
 cs = OCIO.ColorSpace(name='Linear sRGB/709')
@@ -51,8 +48,8 @@ cs.setDescription("Scene-linear, BT.REC.709 / sRGB primaries, high dynamic range
 cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
 cs.setAllocation(OCIO.Constants.ALLOCATION_LG2)
 cs.setAllocationVars([-15.0, 6.0])
+cs.setFamily("sRGB/BT.709")
 config.addColorSpace(cs)
-
 
 ###############################################################################
 
@@ -66,10 +63,6 @@ def fromSRGB(v):
         return v/12.92
     return ((v + .055) / 1.055) ** 2.4
 
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
 NUM_SAMPLES = 2**12+5
 RANGE = (0.0, 1.0)
 data = []
@@ -78,7 +71,7 @@ for i in xrange(NUM_SAMPLES):
     x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
     data.append(fromSRGB(x))
 
-# Data is srgb->linear
+# Data is sRGB Transfer -> Linear sRGB / 709
 WriteSPI1D('luts/srgb.spi1d', RANGE[0], RANGE[1], data)
 
 cs = OCIO.ColorSpace(name='sRGB')
@@ -87,7 +80,7 @@ cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
 cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
 cs.setAllocationVars([RANGE[0], RANGE[1]])
 
-t = OCIO.FileTransform('srgb.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
+t = OCIO.FileTransform('srgb.spi1d', interpolation=OCIO.Constants.INTERP_BEST)
 cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
 config.addColorSpace(cs)
 
@@ -100,7 +93,7 @@ for i in xrange(NUM_SAMPLES):
     x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
     data.append(fromSRGB(x))
 
-# Data is srgb->linear
+# Data is sRGB Transfer -> Linear sRGB / 709
 WriteSPI1D('luts/srgbf.spi1d', RANGE[0], RANGE[1], data)
 
 
@@ -110,7 +103,7 @@ cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
 cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
 cs.setAllocationVars([RANGE[0], RANGE[1]])
 
-t = OCIO.FileTransform('srgbf.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
+t = OCIO.FileTransform('srgbf.spi1d', interpolation=OCIO.Constants.INTERP_BEST)
 cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
 config.addColorSpace(cs)
 
@@ -128,10 +121,10 @@ def fromRec709(v):
 
 # These samples and range have been chosen to write out this colorspace with
 # a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
+# crossings.
 
 NUM_SAMPLES = 2**12+5
-RANGE = (0.0, 1.0)
+RANGE = (-0.125, 1.125)
 data = []
 for i in xrange(NUM_SAMPLES):
     x = i/(NUM_SAMPLES-1.0)
@@ -147,9 +140,37 @@ cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
 cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
 cs.setAllocationVars([RANGE[0], RANGE[1]])
 
-t = OCIO.FileTransform('rec709fr.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
+t = OCIO.FileTransform('rec709fr.spi1d', interpolation=OCIO.Constants.INTERP_BEST)
 cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
 config.addColorSpace(cs)
+
+###############################################################################
+
+# This seems like an unfortunate hack. Unfortunately, when transforming from
+# a larger gamut to a smaller gamut via XYZ, the values that result can
+# be negative and >1.0. While the negatives aren't an issue, the values
+# greater than one do not represent the same model metrics. In an HDR
+# radiometrically linear model, the values above 1.0 are nothing more than a
+# more luminous representation of the same primary values below 1.0. When
+# transferring from XYZ, those values are a spill over of all ranges that
+# cannot be represented in the smaller gamut. To this end, we must clamp
+# those values.
+
+def toClamp(v):
+    if v<=0.0:
+        return 0
+    elif v>=1.0:
+        return 1.0
+    return v
+
+NUM_SAMPLES = 2**16+25
+RANGE = (-0.000000001, 1.0)
+data = []
+for i in xrange(NUM_SAMPLES):
+    x = i/(NUM_SAMPLES-1.0)
+    data.append(toClamp(x))
+
+WriteSPI1D('luts/clamp.spi1d', RANGE[0], RANGE[1], data)
 
 ###############################################################################
 
@@ -165,10 +186,9 @@ RANGE = (0, 1)
 data = []
 for i in xrange(NUM_SAMPLES):
     x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
     data.append(fromAdobeRGBtransfer(x))
 
-# Data is AdobeRGBtransfer->Linear
+# Data is AdobeRGB Transfer -> Linear sRGB / 709
 WriteSPI1D('luts/adobergb_transfer_to_lin.spi1d', RANGE[0], RANGE[1], data)
 
 cs = OCIO.ColorSpace(name='AdobeRGB D65')
@@ -178,308 +198,14 @@ cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
 cs.setAllocationVars([RANGE[0], RANGE[1]])
 
 groupTransform = OCIO.GroupTransform()
-groupTransform.push_back(OCIO.FileTransform('adobergb_transfer_to_lin.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR))
+groupTransform.push_back(OCIO.FileTransform('adobergb_transfer_to_lin.spi1d', interpolation=OCIO.Constants.INTERP_BEST))
 groupTransform.push_back(OCIO.FileTransform('adobergb_to_xyz.spimtx'))
-groupTransform.push_back(OCIO.FileTransform('srgb_to_xyz.spimtx',direction=OCIO.Constants.TRANSFORM_DIR_INVERSE))
+groupTransform.push_back(OCIO.FileTransform('srgb_to_xyz.spimtx', interpolation=OCIO.Constants.INTERP_BEST, direction=OCIO.Constants.TRANSFORM_DIR_INVERSE))
+groupTransform.push_back(OCIO.FileTransform('clamp.spi1d', interpolation=OCIO.Constants.INTERP_BEST))
 
 cs.setTransform(groupTransform, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
 config.addColorSpace(cs)
 
-###############################################################################
-
-"""
-cineonBlackOffset = 10.0 ** ((95.0 - 685.0)/300.0)
-
-def fromCineon(x):
-    return (10.0**((1023.0 * x - 685.0) / 300.0) - cineonBlackOffset) / (1.0 - cineonBlackOffset)
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromCineon(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/cineon.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='Cineon')
-cs.setDescription("Cineon (Log Film Scan)")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('cineon.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-
-###############################################################################
-
-cs = OCIO.ColorSpace(name='Gamma1.8')
-cs.setDescription("Emulates a idealized Gamma 1.8 display device.")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([0.0, 1.0])
-
-t = OCIO.ExponentTransform(value=(1.8,1.8,1.8,1.0))
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-cs = OCIO.ColorSpace(name='Gamma2.2')
-cs.setDescription("Emulates a idealized Gamma 2.2 display device.")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([0.0, 1.0])
-
-t = OCIO.ExponentTransform(value=(2.2,2.2,2.2,1.0))
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-###############################################################################
-
-
-# Log to Linear light conversions for Panalog
-# WARNING: these are estimations known to be close enough.
-# The actual transfer functions are not published
-
-panalogBlackOffset = 10.0 ** ((64.0 - 681.0) / 444.0)
-
-def fromPanalog(x):
-    return (10.0**((1023 * x - 681.0) / 444.0) - panalogBlackOffset) / (1.0 - panalogBlackOffset)
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromPanalog(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/panalog.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='Panalog')
-cs.setDescription("Sony/Panavision Genesis Log Space")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('panalog.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-
-###############################################################################
-
-
-
-redBlackOffset = 10.0 ** ((0.0 - 1023.0) / 511.0)
-
-def fromREDLog(x):
-    return ((10.0 ** ((1023.0 * x - 1023.0) / 511.0)) - redBlackOffset) / (1.0 - redBlackOffset)
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromREDLog(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/redlog.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='REDLog')
-cs.setDescription("RED Log Space")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('redlog.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-
-###############################################################################
-
-def fromViperLog(x):
-    return 10.0**((1023.0 * x - 1023.0) / 500.0)
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromViperLog(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/viperlog.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='ViperLog')
-cs.setDescription("Viper Log Space")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('viperlog.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-
-###############################################################################
-
-
-alexav3logc_a = 5.555556
-alexav3logc_b = 0.052272
-alexav3logc_c = 0.247190
-alexav3logc_d = 0.385537
-alexav3logc_e = 5.367655
-alexav3logc_f = 0.092809
-alexav3logc_cut = 0.010591
-alexav3logc_eCutF = alexav3logc_e*alexav3logc_cut + alexav3logc_f
-
-# This corresponds to EI800 per Arri Doc
-# http://www.arridigital.com/forum/index.php?topic=6372.0
-# http://www.arri.com/?eID=registration&file_uid=7775
-def fromAlexaV3LogC(x):
-    if x > alexav3logc_eCutF:
-        return (10.0 **((x - alexav3logc_d) / alexav3logc_c) - alexav3logc_b) / alexav3logc_a
-    else:
-        return (x - alexav3logc_f) / alexav3logc_e
-
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromAlexaV3LogC(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/alexalogc.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='AlexaV3LogC')
-cs.setDescription("Alexa Log C")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('alexalogc.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-###############################################################################
-
-'PLogLin'
-
-# Josh Pines style pivoted log/lin conversion
-minLinValue = 1e-10
-linReference = 0.18
-logReference = 445.0
-negativeGamma = 0.6
-densityPerCodeValue = 0.002
-ngOverDpcv = negativeGamma/densityPerCodeValue
-dpcvOverNg = densityPerCodeValue/negativeGamma
-
-def fromPLogLin(x):
-    return (10.0**((x*1023.0 - logReference)*dpcvOverNg ) * linReference)
-
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromPLogLin(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/ploglin.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='JPLogLin')
-cs.setDescription("Josh Pines style pivoted log/lin conversion. 445->0.18")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('ploglin.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-###############################################################################
-
-def fromSLog(x):
-    return (10.0 ** (((x - 0.616596 - 0.03) / 0.432699)) - 0.037584)
-
-# These samples and range have been chosen to write out this colorspace with
-# a limited over/undershoot range, which also exactly samples the 0.0,1.0
-# crossings
-
-NUM_SAMPLES = 2**12+5
-RANGE = (-0.125, 1.125)
-data = []
-for i in xrange(NUM_SAMPLES):
-    x = i/(NUM_SAMPLES-1.0)
-    x = Fit(x, 0.0, 1.0, RANGE[0], RANGE[1])
-    data.append(fromSLog(x))
-
-# Data is srgb->linear
-WriteSPI1D('luts/slog.spi1d', RANGE[0], RANGE[1], data)
-
-cs = OCIO.ColorSpace(name='SLog')
-cs.setDescription("Sony SLog")
-cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
-cs.setAllocation(OCIO.Constants.ALLOCATION_UNIFORM)
-cs.setAllocationVars([RANGE[0], RANGE[1]])
-
-t = OCIO.FileTransform('slog.spi1d', interpolation=OCIO.Constants.INTERP_LINEAR)
-cs.setTransform(t, OCIO.Constants.COLORSPACE_DIR_TO_REFERENCE)
-config.addColorSpace(cs)
-
-
-###############################################################################
-
-'REDSpace'
-
-
-"""
 ###############################################################################
 
 cs = OCIO.ColorSpace(name='Raw / Data')
@@ -487,7 +213,6 @@ cs.setDescription("Raw Data. Used for normals, points, etc.")
 cs.setBitDepth(OCIO.Constants.BIT_DEPTH_F32)
 cs.setIsData(True)
 config.addColorSpace(cs)
-
 
 ###############################################################################
 
@@ -503,10 +228,7 @@ config.addDisplay(display, 'None', 'Raw / Data')
 config.setActiveViews(','.join(['sRGB', 'BT.709 (FR)', 'None']))
 config.setActiveDisplays(','.join(['sRGB Monitor', 'REC.709 Monitor', 'No Transform']))
 
-
 ###############################################################################
-
-
 
 try:
     config.sanityCheck()
@@ -517,6 +239,4 @@ f = file(outputfilename,"w")
 f.write(config.serialize())
 f.close()
 print "Wrote",outputfilename
-
-# Core/LUT/include/LUT/fnLUTConversions.h
 
